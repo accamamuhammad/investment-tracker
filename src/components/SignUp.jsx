964495812dd8971d9a935b2552b6359d7fb876fd
accamamuhammad@gmail.com <accamamuhammad@gmail.com>
2024-10-10 14:47:51 +0100

    .

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { auth } from "../config/firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
