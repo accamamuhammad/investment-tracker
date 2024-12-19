6d1e757a4a8650d165415ce2c84ab5388ca099d4
accama <accamamuhammad@gmail.com>
2024-12-19 11:52:54 +0100

    .

---
+++ src/FakeData.js
@@ -0,0 +1,19 @@
+const pieChartData = {
+  labels: ["Facebook", "Instagram", "Twitter", "YouTube", "LinkedIn"],
+  datasets: [
+    {
+      label: "Time Spent",
+      data: [120, 60, 30, 90, 45],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+export default pieChartData;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,11 @@ import InvestmentBox from "./InvestmentBox";
 import { app, auth } from "../config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { ref, get, getDatabase } from "firebase/database";
+import FakeData from "../FakeData";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJs, Tooltip, ArcElement, plugins } from "chart.js";
+
+ChartJs.register(Tooltip, ArcElement);
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -19,6 +24,10 @@ const Home = () => {
   let allLosses = [];
   let allInvestment = [];
   let allReturnOnInvestment = [];
+  const options = {
+    responsive: true,
+    plugins: {},
+  };
 
   //* Check if user is logged in
   useEffect(() => {
@@ -107,15 +116,6 @@ const Home = () => {
     calculateTotalInvested();
   }, [data]);
 
-  //* Check if they is data in
-  useEffect(() => {
-    if (data.length === 0) {
-      console.log("empty array");
-    } else {
-      console.log("not empty array");
-    }
-  }, []);
-
   return (
     <main className="w-screen h-fit bg-newBlue flex items-center justify-center">
       <div className="w-full sm:w-[385px] h-screenHeight overflow-auto p-7 bg-white rounded-lg gap-5 flex flex-col items-center justify-between">
@@ -127,22 +127,8 @@ const Home = () => {
           </h1>
         </div>
         <div>
-          <div className="w-56 h-56 rounded-full bg-newBlue flex items-center justify-center">
-            {userLogInStatus ? (
-              <div>
-                <h1 className="font-semibold text-xs opacity-55">Piechart</h1>
-              </div>
-            ) : (
-              <div className="space-y-5">
-                <h1 className="px-1 text-center font-semibold text-xs opacity-55">
-                  Click the "🚀" To add new investment
-                </h1>
-                <h1 className="px-1 text-center font-semibold text-xs opacity-55">
-                  Click the "⚙️" To access settings
-                </h1>
-              </div>
-            )}
-          </div>
+          {/* Pie Chart */}
+          <Pie options={options} data={FakeData} />
         </div>
         <div className="w-full flex flex-row gap-5 items-center justify-between">
           <DisplayBox icon="📉" title="Loss" total={totalLosses} />
