c422b02c25a03c34930946cabaee3df6456c976a
accama <accamamuhammad@gmail.com>
2024-12-19 13:53:40 +0100

    pie chart setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,6 @@ import InvestmentBox from "./InvestmentBox";
 import { app, auth } from "../config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { ref, get, getDatabase } from "firebase/database";
-import FakeData from "../FakeData";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJs, Tooltip, ArcElement, plugins } from "chart.js";
 
@@ -18,6 +17,13 @@ const Home = () => {
   const [totalInvestment, setTotalInvestment] = useState(0);
   const [totalLosses, setTotalLosses] = useState(0);
   const [userLogInStatus, setUserLogInStatus] = useState(false);
+  const [investmentLabels, setInvestmentLabels] = useState([
+    "Dogs",
+    "Rocky Rabbit",
+    "MemeFi",
+    "WatCoin",
+  ]);
+  const [investmentAmounts, setInvestmentAmounts] = useState([120, 60, 75, 90]);
   let lossSum = 0;
   let returnSum = 0;
   let investmentSum = 0;
@@ -28,6 +34,32 @@ const Home = () => {
     responsive: true,
     plugins: {},
   };
+
+  //* Pie Chart Data
+  const pieChartData = {
+    labels: investmentLabels,
+    datasets: [
+      {
+        label: "Investments",
+        data: investmentAmounts,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  //* Categorize PieChart data
+  useEffect(() => {
+    // data.map((item, index) => {
+    //   console.log(item);
+    // });
+  }, []);
 
   //* Check if user is logged in
   useEffect(() => {
@@ -128,7 +160,7 @@ const Home = () => {
         </div>
         <div>
           {/* Pie Chart */}
-          <Pie options={options} data={FakeData} />
+          <Pie options={options} data={pieChartData} />
         </div>
         <div className="w-full flex flex-row gap-5 items-center justify-between">
           <DisplayBox icon="📉" title="Loss" total={totalLosses} />
